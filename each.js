3e75c44bdbc70784b9d37abb6f408f3f4e55f9ba
Devanaboina Ashok <19pa1a0437@vishnu.edu.in>
2024-03-09 16:31:00 +0530

    fdf

--- each.js
+++ each.js
@@ -5,5 +5,4 @@ export function each(elements,cb){
     for(let index=0 ;index<elements.length;index++){
         elements[index] = cb(elements[index],index,elements);
     }
-    return elements;
 }
